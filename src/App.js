866d5f91adafd61363c66702f50e26ae7846e588
Gabriel <Gabriel@DESKTOP-GGROU04>
2020-04-26 09:22:11 -0300

    Curso React - Aula 75 - Criando components para ShopPage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import HomePage from './pages/homepage/homepage.component'
 import {Switch, Route} from 'react-router-dom'
 
+import HomePage from './pages/homepage/homepage.component'
+import ShopPage from './pages/shop/shop.component'
 
-function Hats() {
-  return (
-    <div>
-      <h1>HATS</h1>
-    </div>
-  );
-}
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage}/> 
-        <Route exact path="/hats" component={Hats}/> 
+        <Route exact path="/shop" component={ShopPage}/> 
       </Switch>
     </div>
   );
