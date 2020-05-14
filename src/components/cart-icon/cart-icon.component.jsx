66abeb7c86f08ed6b4508f46eadd9daa963867de
Gabriel <Gabriel@DESKTOP-GGROU04>
2020-05-14 16:38:01 -0300

    Curso React - Aula 110 - Adicionando toogle para carlist

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,14 +1,26 @@
 import React from 'react'
+
+//Redux
+import {connect} from 'react-redux'
+import {toogleCartHidden} from '../../redux/card/card.actions'
+
 import './cart-icon.styles.scss'
 import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg'
 
 
-const CartIcon = () => (
-    <div className="cart-icon">
+const CartIcon = ({toogleCartHidden}) => (
+    <div className="cart-icon" onClick={toogleCartHidden}>
         <ShoppingIcon className="shopping-icon"></ShoppingIcon>
         <span className="item-count">0</span>
     </div>
 )
 
 
-export default CartIcon;
+const mapDispatchToProps = dispacth => ({
+    toogleCartHidden: () => dispacth(toogleCartHidden())
+})
+
+export default connect(
+    null,
+    mapDispatchToProps
+) (CartIcon);
---
+++ src/redux/card/card.actions.js
@@ -0,0 +1,6 @@
+import CartActionsTypes from './card.types'
+
+
+export const toogleCartHidden = () =>({
+    type: CartActionsTypes.TOGGLE_CART_HIDDEN
+})
