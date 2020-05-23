00e585fe5553b6ef217e73de4d3709ae71621c90
Gabriel <Gabriel@DESKTOP-GGROU04>
2020-05-23 10:25:36 -0300

    Curso React - Aula 125 - Adicionando setas de aumenta e diminui

--- src/redux/card/card.actions.js
+++ src/redux/card/card.actions.js
@@ -8,4 +8,16 @@ export const toogleCartHidden = () =>({
 export const addItem = item => ({
     type: CartActionsTypes.ADD_ITEM,
     payload: item
+});
+
+export const removeItem = item =>({
+    type: CartActionsTypes.REMOVE_ITEM,
+    payload: item
 })
+
+
+export const clearItemFromCart = item =>({
+    type: CartActionsTypes.CLEAR_ITEM_FROM_CART,
+    payload: item
+})
+
--- src/redux/card/card.utils.js
+++ src/redux/card/card.utils.js
@@ -14,3 +14,24 @@ export const addItemToCart = (cartItems, cartItemToAdd) =>{
     //retorna 1 por que caso ja existir o codigo vai retornar dentro do if existingCartitem
     return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
 }
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) =>{
+    
+    const existingCartitem = cartItems.find(cartItem =>
+        cartItem.id == cartItemToRemove.id
+    )
+    console.log(existingCartitem)
+    if(existingCartitem.quantity == 1){
+        //remove o item
+        return cartItems.filter(cartItem => cartItem.id != cartItemToRemove.id)
+    }
+
+    return cartItems.map(
+        cartItem => 
+        cartItem.id == cartItemToRemove.id
+        ?
+        {...cartItem, quantity: cartItem.quantity - 1}
+        : cartItem
+
+    )
+}
